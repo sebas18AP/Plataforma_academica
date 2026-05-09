92677547bbb566aa8e071924d58742c7def45366
sebas18AP <j.alzate6@utp.edu.co>
2026-05-09 16:08:59 -0500

    Creacion de modelos POO
    Se crearon las clases de Estudiante, Asignatura, Matricula, Calificacion.

---
+++ models/estudiante.py
@@ -0,0 +1,8 @@
+class Estudiante:
+    def __init__(self, identificacion, nombre, correo):
+        self.identificacion = identificacion
+        self.nombre = nombre
+        self.correo = correo
+
+    def mostrar_info(self):
+        return f"Estudiante: {self.nombre} - ID: {self.identificacion}"
