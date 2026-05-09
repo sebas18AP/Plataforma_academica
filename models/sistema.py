927c021ec6a32926640741c56f887b75e790babf
sebas18AP <j.alzate6@utp.edu.co>
2026-05-09 17:40:51 -0500

    Implementación de autenticación de usuarios y roles
    Se actualiza la clase sistemas por que se tuvo que incluir la validación de credenciales y perfiles de profesor/estudiante.

--- models/sistema.py
+++ models/sistema.py
@@ -1,16 +1,30 @@
 class SistemaAcademico:
-    def __init__(self):
-        # Aquí guardamos los datos temporalmente mientras el programa está abierto
+  
+   def __init__(self):
+        # Aquí se guardan los datos temporalmente mientras el programa está abierto
         self.estudiantes = []
         self.asignaturas = []
         self.matriculas = []
         self.calificaciones = []
+        
+        #  Sistema  de usuarios (Usuario: {contraseña, rol})
+        self.usuarios_registrados = {
+            "profesor_johan": {"password": "admin123", "rol": "profesor"},
+            "estudiante_01": {"password": "pass", "rol": "estudiante"}
+        }
+
+    # : Función para que la GUI valide el Login
+    def iniciar_sesion(self, usuario, password):
+        if usuario in self.usuarios_registrados:
+            if self.usuarios_registrados[usuario]["password"] == password:
+                return self.usuarios_registrados[usuario]["rol"]
+        return None
 
     def registrar_estudiante(self, estudiante):
         self.estudiantes.append(estudiante)
         return f"Estudiante {estudiante.nombre} registrado con éxito."
 
-    # Aquí es donde tú brillas con las estadísticas que pide el profesor
+    # Estadisticas 
     def calcular_promedio_estudiante(self, identificacion):
         # Filtramos solo las calificaciones que le pertenecen a este estudiante
         notas = [c.nota_obtenida for c in self.calificaciones if c.estudiante_id == identificacion]
@@ -22,7 +36,7 @@ class SistemaAcademico:
         return round(promedio, 2)
 
         def calcular_promedio_asignatura(self, codigo_asignatura):
-        # Filtramos las notas de una materia específica
+        # aca se filtran las notas de una materia específica
         notas = [c.nota_obtenida for c in self.calificaciones if c.codigo_asignatura == codigo_asignatura]
         
         if not notas:
@@ -32,7 +46,7 @@ class SistemaAcademico:
         return round(promedio, 2)
 
     def obtener_distribucion_notas(self):
-        # Un conteo rápido para saber cuántos pasaron y cuántos reprobaron
+        # se cuentan cunatos pasaron y cuantos perdieron.
         # Suponiendo que se aprueba con 3.0
         aprobados = len([c for c in self.calificaciones if c.nota_obtenida >= 3.0])
         reprobados = len([c for c in self.calificaciones if c.nota_obtenida < 3.0])
